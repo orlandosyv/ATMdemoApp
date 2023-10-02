a0a17ab597ebf6ef6f19c7fd9e0ea9a2f8e15e47
OrlandoYV <sergio_oyv@hotmail.com>
2023-10-02 16:37:12 -0500

    oy - user interface options - final version

--- ATMdemoApp/Program.cs
+++ ATMdemoApp/Program.cs
@@ -41,22 +41,91 @@ public class cardHolder {
             Console.WriteLine("How much do you want to deposit? ");
             double deposit = Double.Parse(Console.ReadLine());
             currentUser.setBalance(currentUser.balance + deposit);
-            Console.WriteLine("Thank you ⸜(｡˃ ᵕ ˂ )⸝, your new balance is: " + currentUser.balance);
+            Console.WriteLine("Thank you, your new balance is: " + currentUser.balance);
+            Console.WriteLine("---------------------------");
         }
 
         void withdraw(cardHolder currentUser)
         {
             Console.WriteLine("How much $$ would you like to withdraw");
-            double withdraw = Double.Parse(Console.ReadLine());
-            if (currentUser.balance >= withdraw) { currentUser.setBalance(currentUser.balance - withdraw); }
-            else { Console.WriteLine("Insufficient balance... ꃋᴖꃋ"); }            
+            double withdrawal = Double.Parse(Console.ReadLine());
+            if (currentUser.balance >= withdrawal) { 
+                currentUser.setBalance(currentUser.balance - withdrawal);
+                Console.WriteLine("You are good to go... Thank you! •ᴗ•");
+                Console.WriteLine("---------------------------");
+            }
+            else 
+            { 
+                Console.WriteLine("Insufficient balance...");
+                Console.WriteLine("---------------------------");
+            }            
         }
 
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Your Current Balance: " + currentUser.balance);
+            Console.WriteLine("---------------------------");
+        }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("4655-4571-8841-1123", 1234, "Richy", "Lagos", 99));
+        cardHolders.Add(new cardHolder("4655-4571-8841-1124", 4567, "Oreja", "Flores", 254.3));
+        cardHolders.Add(new cardHolder("4655-4571-8841-1125", 5678, "Jamie", "Vardy", 185.1));
+        cardHolders.Add(new cardHolder("4655-4571-8841-1126",6789 , "Claudio", "Pizarro", 185.1));
+
+        //Prompt User
+        Console.WriteLine("Welcome to ATM Demo APP done by OrlandoSYV Corp ");
+        Console.WriteLine("Please enter your Card number: ");
+        String debitCardNum = "";
+        cardHolder currentUser;
+        while(true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                // check in our data set (db in real life) 
+                currentUser = cardHolders.FirstOrDefault(ch => ch.cardNum == debitCardNum);
+                if(currentUser != null)
+                { Console.WriteLine("Card recognized ");
+                  break; }
+                else { Console.WriteLine("Card NOT recognized ⛔"); }
+                
+            }
+            catch { Console.WriteLine("Card NOT recognized ⛔"); }
+        }
+        Console.WriteLine("Enter you PIN number: ");
+        int userPIN = 0;
+        while (true)
+        {
+            try
+            {
+                userPIN = int.Parse(Console.ReadLine());
+                if (currentUser.getPIN() == userPIN) {
+                    Console.WriteLine("Correct PIN ");
+                    break; }
+                else { Console.WriteLine("Incorrect PIN "); }
+            }
+            catch { Console.WriteLine("Incorrect PIN "); }
+        }
+
+        Console.WriteLine("Welcome " + currentUser.getFirstName() + " :D");
+        int option = 0;
+        do 
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch { }
+            if (option == 1) { deposit(currentUser); }
+            else if (option == 2) { withdraw(currentUser); }
+            else if (option == 3) { balance(currentUser); }
+            else if (option == 4) { break; }
+            else { option = 0; }
+        }
+        while (option != 4);
+        Console.WriteLine("Thank you, have a nice day!");
     }
-    
-
-
-    
-    
-   
 }
