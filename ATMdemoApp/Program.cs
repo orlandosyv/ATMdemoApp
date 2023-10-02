4270b8665c0502b2a9152d86056d3e5e43518cb5
sergi <sergi@ASUSVIVO>
2023-10-02 14:20:49 -0500

    Add project files.

---
+++ ATMdemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class cardHolder {
+    string cardNum;
+    int PIN;
+    string firstName;
+    string lastName;
+    double balance;
+    public cardHolder(string cardNum, int PIN, string firstName, string lastName, double balance ) {
+        this.cardNum = cardNum;
+        this.PIN = PIN;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+   
+    
+    
+   
+}
